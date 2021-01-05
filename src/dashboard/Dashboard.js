10aa2698f2a1264ccd4fe33efdde14c940ec3b58
alterclown <rahi2721@gmail.com>
2021-01-05 14:44:33 +0600

    dash

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -114,7 +114,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Header() {
+export default function Dashboard() {
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
   const handleDrawerOpen = () => {
